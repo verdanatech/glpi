721ceb85b652e2b133ced7490160ac8364bab47a
Márcio Fernandes <marcio.fernandes@verdanatech.com>
2023-04-05 10:54:07 -0300

    :sparkles: Implement Log in Plugins

--- front/plugin.form.php
+++ front/plugin.form.php
@@ -36,7 +36,7 @@
 /**
  * @since 0.84
  */
-
+use Glpi\Event;
 include('../inc/includes.php');
 
 Session::checkRight("config", UPDATE);
@@ -45,6 +45,11 @@ $plugin = new Plugin();
 
 $id     = isset($_POST['id']) && is_numeric($_POST['id']) ? (int)$_POST['id'] : null;
 $action = $id > 0 && isset($_POST['action']) ? $_POST['action'] : null;
+
+# Implements log
+$plugin_informations = current($plugin->find(["id" => $id]));
+Log::history($id, 'Plugins', ['0','',$action]);
+Event::log(0, 'system', 1, 'Plugins', sprintf(__('%s %s plugin %s'), $_SESSION["glpiname"], $action, $plugin_informations['directory']));
 
 switch ($action) {
     case 'install':
