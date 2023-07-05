ae0f4a4789d412f11b36e87b1d1f8170b6795014
Márcio Fernandes <marcio.fernandes@verdanatech.com>
2023-07-05 11:28:26 -0300

    :sparkles: Add Favicon  #24

--- index.php
+++ index.php
@@ -131,10 +131,21 @@ if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
         Toolbox::manageRedirect($redirect);
     }
 
+    $plugin = new Plugin();
+    if ($plugin->isInstalled('skins') && $plugin->isActivated('skins')) {
+        $image =  PluginSkinsConfig::returnImgMenus('favicon');
+        $skins = true;
+    } else {
+        $skins = false;
+        $image = null;
+    }
+    
     // Random number for html id/label
     $rand = mt_rand();
 
     TemplateRenderer::getInstance()->display('pages/login.html.twig', [
+        'plugin_skins'        => $skins,
+        'skins_image'         => $image,
         'rand'                => $rand,
         'card_bg_width'       => true,
         'lang'                => $CFG_GLPI["languages"][$_SESSION['glpilanguage']][3],
