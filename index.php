607f59c591ca8d3504513dac7e7ecd72e8693b33
verdanatech-jadiael <jadiael.monteiro@verdanatech.com>
2024-03-13 20:22:17 -0300

    feat(Style): Add Styles Improvements

--- index.php
+++ index.php
@@ -133,8 +133,18 @@ if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
 
     // Random number for html id/label
     $rand = mt_rand();
+    $plugin = new Plugin();
+    if ($plugin->isInstalled('skins') && $plugin->isActivated('skins')) {
+        $image =  PluginSkinsConfig::returnImgMenus('favicon');
+        $skins = true;
+    } else {
+        $skins = false;
+        $image = null;
+    }
 
     TemplateRenderer::getInstance()->display('pages/login.html.twig', [
+        'plugin_skins'        => $skins,
+        'skins_image'         => $image,
         'rand'                => $rand,
         'card_bg_width'       => true,
         'lang'                => $CFG_GLPI["languages"][$_SESSION['glpilanguage']][3],
@@ -146,11 +156,11 @@ if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
         'pwdfield'            => ($_SESSION['pwdfield'] = uniqid('fieldb')),
         'rmbfield'            => ($_SESSION['rmbfield'] = uniqid('fieldc')),
         'show_lost_password'  => $CFG_GLPI["notifications_mailing"]
-                              && countElementsInTable('glpi_notifications', [
-                                  'itemtype'  => 'User',
-                                  'event'     => 'passwordforget',
-                                  'is_active' => 1
-                              ]),
+            && countElementsInTable('glpi_notifications', [
+                'itemtype'  => 'User',
+                'event'     => 'passwordforget',
+                'is_active' => 1
+            ]),
         'languages_dropdown'  => Dropdown::showLanguages('language', [
             'display'             => false,
             'rand'                => $rand,
@@ -159,8 +169,8 @@ if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
             'width'               => '100%'
         ]),
         'right_panel'         => strlen($CFG_GLPI['text_login']) > 0
-                               || count($PLUGIN_HOOKS[Hooks::DISPLAY_LOGIN] ?? []) > 0
-                               || $CFG_GLPI["use_public_faq"],
+            || count($PLUGIN_HOOKS[Hooks::DISPLAY_LOGIN] ?? []) > 0
+            || $CFG_GLPI["use_public_faq"],
         'auth_dropdown_login' => Auth::dropdownLogin(false, $rand),
         'copyright_message'   => Html::getCopyrightMessage(false),
         'errors'              => $errors
