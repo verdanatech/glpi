aaac76e2cc49b444904b3768b41f4cf515e17072
Márcio Fernandes <marcio.fernandes@verdanatech.com>
2024-04-24 14:33:23 -0300

    feat(Style): Add Styles Improvements

--- src/Html.php
+++ src/Html.php
@@ -828,11 +828,24 @@ class Html
                 self::helpHeader(__('Access denied'), '');
             }
         }
+        $plugin = new Plugin();
+        if ($plugin->isInstalled('skins') && $plugin->isActivated('skins') && version_compare(SKINS_VERSION, '3.4.0', '>=')) {
+            if (class_exists('PluginSkinsConfig')) {
+            $image =  PluginSkinsConfig::returnImgMenus('favicon');
+            $skins = true;
+            }
+        } else {
+            $skins = false;
+            $image = null;
+        }
+
 
         TemplateRenderer::getInstance()->display('display_and_die.html.twig', [
             'title'   => __('Access denied'),
             'message' => $message,
             'link'    => Html::getBackUrl(),
+            'plugin_skins' => $skins,
+            'skins_image' => $image
         ]);
 
         self::nullFooter();
@@ -1299,6 +1312,18 @@ HTML;
                 }
             }
         }
+        $plugin = new Plugin();
+        if ($plugin->isInstalled('skins') && $plugin->isActivated('skins') && version_compare(SKINS_VERSION,'3.2.0','>=')) {
+            if (class_exists('PluginSkinsConfig')) {
+            $image =  PluginSkinsConfig::returnImgMenus('favicon');
+            $skins = true;
+            }
+        } else {
+            $skins = false;
+            $image = null;
+        }
+        $tpl_vars['plugin_skins'] = $skins;
+        $tpl_vars['skins_image'] = $image;
         $tpl_vars['css_files'][] = ['path' => 'css/palettes/' . $theme . '.scss'];
 
         $tpl_vars['js_files'][] = ['path' => 'public/lib/base.js'];
@@ -1714,6 +1739,18 @@ HTML;
         $menu_active     = $menu[$sector]['content'][$active_item]['title'] ?? "";
 
         $menu = Plugin::doHookFunction("redefine_menus", $menu);
+        $plugin = new Plugin();
+        if ($plugin->isInstalled('skins') && $plugin->isActivated('skins')) {
+            if (class_exists('PluginSkinsConfig')) {
+            $image = PluginSkinsConfig::returnImgMenus("menu");
+            $image_collapsed = PluginSkinsConfig::returnImgMenus("collapsed-menu");
+            $skins = true;
+            }
+        } else {
+            $skins = false;
+            $image = null;
+            $image_collapsed  = null;
+        }
 
         $tpl_vars = [
             'menu'        => $menu,
@@ -1721,6 +1758,9 @@ HTML;
             'item'        => $item,
             'option'      => $option,
             'menu_active' => $menu_active,
+            'plugin_skins'  => $skins,
+            'image'      => $image,
+            'image_collapsed'      => $image_collapsed
         ];
         $tpl_vars += self::getPageHeaderTplVars();
 
@@ -1963,6 +2003,19 @@ HTML;
         $menu = self::generateHelpMenu();
         $menu = Plugin::doHookFunction("redefine_menus", $menu);
 
+        $plugin = new Plugin();
+        if ($plugin->isInstalled('skins') && $plugin->isActivated('skins') && version_compare(SKINS_VERSION,'3.2.0','>=')) {
+            if (class_exists('PluginSkinsConfig')) {
+            $image = PluginSkinsConfig::returnImgMenus("menu");
+            $image_collapsed = PluginSkinsConfig::returnImgMenus("collapsed-menu");
+            $skins = true;
+            }
+        } else {
+            $skins = false;
+            $image = null;
+            $image_collapsed  = null;
+        }
+
         $tmp_active_item = explode("/", $item);
         $active_item     = array_pop($tmp_active_item);
         $menu_active     = $menu[$sector]['content'][$active_item]['title'] ?? "";
@@ -1972,6 +2025,9 @@ HTML;
             'item'        => $item,
             'option'      => $option,
             'menu_active' => $menu_active,
+            'plugin_skins'  => $skins,
+            'image'      => $image,
+            'image_collapsed'      => $image_collapsed,
         ];
         $tpl_vars += self::getPageHeaderTplVars();
 
