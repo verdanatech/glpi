d3a44466febc8b811f64557eb7befb3ff198a013
Márcio Fernandes <marcio.fernandes@verdanatech.com>
2024-11-07 10:41:40 -0300

    Correction Filter

--- src/Search.php
+++ src/Search.php
@@ -4789,13 +4789,9 @@ JAVASCRIPT;
 
                     // Potential negation will be handled by the subquery operator
                     $SEARCH = self::makeTextSearch($val, false);
-                    if ($searchtype === 'contains') {
-                        $subquery_operator = $nott ? "NOT IN" : "IN";
-                    } else {
-                        $subquery_operator = $nott ? "IN" : "NOT IN";
-                    }
+                    $subquery_operator = $nott ? "NOT IN" : "IN";
                 } else {
-                    $SEARCH = self::makeTextSearch($val, $searchtype === 'contains' ? $nott : !$nott);
+                    $SEARCH = self::makeTextSearch($val, $nott);
                 }
                 break;
 
