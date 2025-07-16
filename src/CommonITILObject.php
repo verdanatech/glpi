593a7198a63bba5cf84e2424c0a160bcb22d0bd8
Marcio Fernandes <marcio.fernandes@verdanatech.com>
2025-07-16 12:17:34 -0300

    Active Task Template Notification

--- src/CommonITILObject.php
+++ src/CommonITILObject.php
@@ -8197,8 +8197,7 @@ abstract class CommonITILObject extends CommonDBTM
             $itiltask->add([
                 '_tasktemplates_id'           => $tasktemplates_id,
                 $this->getForeignKeyField()   => $this->fields['id'],
-                'date'                        => $this->fields['date'],
-                '_disablenotif'               => true,
+                'date'                        => $this->fields['date']
             ]);
         }
     }
