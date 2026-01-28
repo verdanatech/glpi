e7a1ace88849942a9d7597b7c8cda5ecde855acd
Márcio Fernandes <marcio@PEVTCMTNB3313.local>
2026-01-28 11:30:45 -0300

    Add new version

---
+++ SECURITY.md
@@ -0,0 +1,31 @@
+# Security Policy
+
+**⚠️ Please never use standard issues to report security problems; vulnerabilities are never published before a fix release is available. ⚠️**
+
+Critical and high advisories are published one month after the release. All other advisories are published one week after the fix release.
+
+## Reporting a Vulnerability
+
+If you found a security issue, please open an advisory on [github](https://github.com/glpi-project/glpi/security/advisories/new).
+
+If you do not have a github account, or if you want to contact us regarding a security issue, you can drop a mail to \[glpi-security AT ow2.org\].
+
+You should provide us all details about the issue and the way to reproduce it.
+You may also provide a script that can be used to check the issue exists.
+
+Once the report will be handled, and if the issue is not yet fixed (or in progress)
+we'll add it to the GitHub security tab, and add you as observer. Meanwhile,
+you will reserve a CVE for the issue.
+
+Thank you for improving the security of glpi.
+
+## Supported Versions
+
+| Version | Supported          |
+| ------- | ------------------ |
+| 10.0.x  | :heavy_check_mark: |
+| 9.5.x   | :x:                |
+| 9.4.x   | :x:                |
+| 9.3.x   | :x:                |
+| 9.2.x   | :x:                |
+| < 9.2   | :x:                |
---
+++ ajax/itemTicket.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2026 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+include('../inc/includes.php');
+
+header("Content-Type: text/html; charset=UTF-8");
+Html::header_nocache();
+
+Session::checkLoginUser();
+$item_ticket = new Item_Ticket();
+
+switch ($_POST['action']) {
+    case 'add':
+        if (!empty($_POST['my_items'])) {
+            [$_POST['itemtype'], $_POST['items_id']] = explode('_', $_POST['my_items']);
+        }
+        if (!empty($_POST['itemtype']) && !empty($_POST['items_id'])) {
+            $_POST['params']['items_id'][$_POST['itemtype']][$_POST['items_id']] = $_POST['items_id'];
+        }
+        Item_Ticket::itemAddForm(new Ticket(), $_POST['params']);
+        break;
+
+    case 'delete':
+        if (!empty($_POST['itemtype']) && !empty($_POST['items_id'])) {
+            $deleted = true;
+            if ($_POST['params']['id'] > 0) {
+                $criteria = [
+                    'tickets_id' => $_POST['params']['id'],
+                    'items_id' => $_POST['items_id'],
+                    'itemtype' => $_POST['itemtype'],
+                ];
+                if (
+                    $item_ticket->getFromDBByCrit($criteria)
+                    && $item_ticket->can($item_ticket->getID(), DELETE)
+                ) {
+                    $deleted = $item_ticket->delete(['id' => $item_ticket->getID()]);
+                }
+            }
+            if ($deleted) {
+                unset($_POST['params']['items_id'][$_POST['itemtype']][array_search($_POST['items_id'], $_POST['params']['items_id'][$_POST['itemtype']])]);
+            }
+            Item_Ticket::itemAddForm(new Ticket(), $_POST['params']);
+        }
+
+        break;
+}
