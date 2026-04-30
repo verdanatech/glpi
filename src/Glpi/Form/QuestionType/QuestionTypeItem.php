6721a71743cafd0ec9dc1a17a974cd6f85e4328f
Marcio Fernandes <marcio.fernandes@verdanatech.com>
2026-04-30 10:30:16 -0300

    Correction in Ticket Content

--- src/Glpi/Form/QuestionType/QuestionTypeItem.php
+++ src/Glpi/Form/QuestionType/QuestionTypeItem.php
@@ -403,6 +403,10 @@ class QuestionTypeItem extends AbstractQuestionType implements
         }
 
         $name = $item->fields['name'];
+        if(array_key_exists('completename',$item->fields)){
+            $name = $item->fields['completename'];
+
+        }
 
         // Append additional fields to match what is displayed in renderEndUserTemplate.
         $itemtype = $answer['itemtype'];
