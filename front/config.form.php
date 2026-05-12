37179a52a9f7032a009352aeb79887377e3f61e7
Marcio Fernandes <marcio.fernandes@verdanatech.com>
2026-05-12 14:29:42 -0300

    Correction Config

--- front/config.form.php
+++ front/config.form.php
@@ -49,7 +49,7 @@ if (isset($_GET['check_version'])) {
 
 $config = new Config();
 $_POST['id'] = Config::getConfigIDForContext('core');
-if (!empty($_POST["update_auth"])) {
+if (isset($_POST["update_auth"])) {
     Session::checkRight("config", UPDATE);
     $config->update($_POST);
     Html::back();
